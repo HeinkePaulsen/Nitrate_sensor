b691b6aea53935d744223e5a14d088cd42c203f5
HeinkePaulsen <155008624+HeinkePaulsen@users.noreply.github.com>
2026-02-26 15:26:30 +0100

    Update comments for clarity in setAddress.py

--- software/setAddress.py
+++ software/setAddress.py
@@ -1,18 +1,18 @@
 import serial
 import time
 
-# Öffne die serielle Verbindung
+# open serial terminal, adapt to yours
 with serial.Serial('/dev/tty.usbserial-14620', 9600, timeout=1) as ser:
     try:
-        # Setze die Adresse für das Gerät
+        # set address for the module
         ser.write(b'SetAddress!1\n')
         time.sleep(0.1)  # Kleine Pause, um sicherzustellen, dass das Gerät genug Zeit hat
         line = ser.readline().decode('utf-8').strip()
         print(f"Response to SetAddress: {line}")
 
-        # Hole die aktuelle Adresse des Geräts
+        # get address for the module
         ser.write(b'GetAddress!\n')
-        time.sleep(0.1)  # Kleine Pause, um sicherzustellen, dass das Gerät genug Zeit hat
+        time.sleep(0.1)  # small break
         line = ser.readline().decode('utf-8').strip()
         print(f"Current Address: {line}")
     except serial.SerialException as e:
