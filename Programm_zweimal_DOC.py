c7934c9b50e46d043dc9ebcb1b35a56a426819cf
HeinkePaulsen <155008624+HeinkePaulsen@users.noreply.github.com>
2026-02-26 15:28:14 +0100

    Translated comments from German to English in Programm_zweimal_DOC.py

---
+++ Programm_zweimal_DOC.py
@@ -0,0 +1,23 @@
+# Global variables
+
+# Function to start measurements
+def start_measurements():
+    # Start measurement routine
+    pass
+
+# Function to read sensor data
+def read_sensor_data():
+    # Read data from the sensor
+    pass
+
+# Function to stop measurements
+
+def stop_measurements():
+    # Stop the measurement routine
+    pass
+
+# Function to process the data
+
+def process_data(data):
+    # Process the incoming data
+    pass
